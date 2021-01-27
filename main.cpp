e3decf770ec407d9dcd156d5f2d75fc92624a187
Borys-karton <59846312+Borys-karton@users.noreply.github.com>
2021-01-27 17:37:35 +0100

    aaa

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
-int Policz(int a,int b){
-    return a+b;
-}
-
 int main() {
-    std::cout << "Branch OU_01, Policz 2+3= " << Policz(2,3) << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
